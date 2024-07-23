1a409c138fe9b31251f93369540967dd4999976b
woody <shlee@smallticket.com>
2024-07-23 21:39:42 +0900

    chore : update application properties (database setting)

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
